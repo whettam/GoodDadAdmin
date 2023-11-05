f275039b77401f9af6e4e74cf7f65b790bcff9ee
whettam <mjballard88@gmail,com>
2023-11-05 01:14:16 -0600

    Changed Navbar title

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-                <a class="navbar-brand" href="#">Admin</a>
+                <a class="navbar-brand" href="#">Good Dads - Admin</a>
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="#">Home</a>
